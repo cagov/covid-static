ebd0f622aaed0001e757f4e1f016abd502364e5a
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-06-19 12:49:11 -0700

    moving files in script

--- src/move.js
+++ src/move.js
@@ -1,16 +1,10 @@
 const fs = require('fs')
 // get the list of files from
-console.log('hello')
 let fileContents = JSON.parse(fs.readFileSync(`${process.env.HOMEDIR}/files.json`,'utf8'));
-console.log(fileContents)
-/*
-  cat $HOME/files.json
-  cat $HOME/files_modified.json
-  cat $HOME/files_added.json
-  cat $HOME/files_removed.json
-  echo '${{ steps.file_changes.outputs.files}}'
-  echo '${{ steps.file_changes.outputs.files_modified}}'
-  echo '${{ steps.file_changes.outputs.files_added}}'
-  echo '${{ steps.file_changes.outputs.files_removed}}'
-*/
-// move them to: - run: cp ./pdf/* path/to/artifact/
+
+// - run: cp ./pdf/19-11069-Natomas-LLC.pdf path/to/artifact/
+fileContents.forEach(file => {
+  if(file.indexOf('pdf/') === 0) {
+    fs.createReadStream(file).pipe(fs.createWriteStream('path/to/artifact/'+file));
+  }
+})
