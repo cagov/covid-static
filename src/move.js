44f3cb931e31e3e20dd5e93f6e9eb7ec9fec23b0
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-06-23 09:11:26 -0700

    check for file exists first

--- src/move.js
+++ src/move.js
@@ -5,6 +5,8 @@ let fileContents = JSON.parse(fs.readFileSync(`${process.env.HOMEDIR}/files.json
 // - run: cp ./pdf/19-11069-Natomas-LLC.pdf path/to/artifact/
 fileContents.forEach(file => {
   if(file.indexOf('pdf/') === 0) {
-    fs.createReadStream(file).pipe(fs.createWriteStream('path/to/artifact/'+file));
+    if(fs.existsSync(file)) {
+      fs.createReadStream(file).pipe(fs.createWriteStream('path/to/artifact/'+file));
+    }    
   }
 })
