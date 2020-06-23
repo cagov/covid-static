2ebcacdca4c851801c6aeaa76059dfff3f177d8f
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-06-23 11:39:42 -0700

    need the pdf name

--- src/move.js
+++ src/move.js
@@ -4,7 +4,7 @@ let fileContents = JSON.parse(fs.readFileSync(`${process.env.HOMEDIR}/files.json
 
 // - run: cp ./pdf/19-11069-Natomas-LLC.pdf path/to/artifact/
 fileContents.forEach(file => {
-  if(file.indexOf('pdf/') === 0) {
+  if(file.indexOf('pdf/') === 0 || file.indexOf('img/') === 0 ) {
     if(fs.existsSync(file)) {
       fs.createReadStream(file).pipe(fs.createWriteStream('path/to/artifact/'+file));
     }
