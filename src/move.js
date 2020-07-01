78e0f55922d04ac620dab0889c80e52163d4ab93
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-07-01 15:45:38 -0700

    fix file moving

--- src/move.js
+++ src/move.js
@@ -3,11 +3,10 @@ const path = require('path');
 // get the list of files from
 let fileContents = JSON.parse(fs.readFileSync(`${process.env.HOMEDIR}/files.json`,'utf8'));
 
-// - run: cp ./pdf/19-11069-Natomas-LLC.pdf path/to/artifact/
 fileContents.forEach(file => {
   if(file.indexOf('pdf/') === 0 || file.indexOf('img/') === 0 ) {
     if(fs.existsSync(file)) {
-      fs.mkdirSync('path/to/artifact/'+file, { recursive: true });
+      fs.mkdirSync(path.dirname('path/to/artifact/'+file), { recursive: true });
       fs.createReadStream(file).pipe(fs.createWriteStream('path/to/artifact/'+file));
     }
   }
