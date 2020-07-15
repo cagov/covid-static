f411bb485d3b4293d0899d2d14d1ddad7f4bd84f
carterm <carter.medlin@gmail.com>
2020-07-15 16:29:26 -0700

    removing priority and freq

--- src/updateSitemap.js
+++ src/updateSitemap.js
@@ -22,16 +22,16 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
 // the root of your website - the protocol and the domain name with a trailing slash
 var root_path = 'http://files.covid19.ca.gov/';
 // XML sitemap generation starts here
-var priority = 0.5;
-var freq = 'monthly';
+//var priority = 0.5;
+//var freq = 'monthly';
 var xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
 for (targetpath of sitemapPaths) {
   for (file of getAllFiles(targetpath)) {
     xml += '<url>';
     xml += '<loc>'+ root_path + file + '</loc>';
-    xml += '<changefreq>'+ freq +'</changefreq>';
-    xml += '<priority>'+ priority +'</priority>';
+    //xml += '<changefreq>'+ freq +'</changefreq>';
+    //xml += '<priority>'+ priority +'</priority>';
     xml += '</url>\n';
   }
 }
