7cf4832bb719b48a6c8eb7a3465923ee57377321
Aaron Hans <aaronhans@gmail.com>
2021-11-09 10:34:54 -0800

    add debug logs to svg pregen

--- src/svg-pregen.js
+++ src/svg-pregen.js
@@ -15,32 +15,38 @@ function writeFile(file, filecontent) {
     waitUntil: 'networkidle2',
   });  
 
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-  await sleep(30*1000);
+  console.log('page loaded');
 
   // write a new date file to make sure script runs even if there is no svg change  
   writeFile('./img/generated/sparklines/date-ran.txt',new Date().toString());
 
   const sparklineVax = await page.$eval("cagov-chart-dashboard-sparkline[data-chart-config-key='vaccines'] svg", el => el.outerHTML);
   writeFile('./img/generated/sparklines/sparkline-vaccines.svg',sparklineVax);
+  console.log('got vaccines');
 
   const sparklineCases = await page.$eval("cagov-chart-dashboard-sparkline[data-chart-config-key='cases'] svg", el => el.outerHTML);
   writeFile('./img/generated/sparklines/sparkline-cases.svg',sparklineCases);
+  console.log('got cases');
 
   const sparklineDeaths = await page.$eval("cagov-chart-dashboard-sparkline[data-chart-config-key='deaths'] svg", el => el.outerHTML);
   writeFile('./img/generated/sparklines/sparkline-deaths.svg',sparklineDeaths);
+  console.log('got deaths');
 
   const sparklineTests = await page.$eval("cagov-chart-dashboard-sparkline[data-chart-config-key='tests'] svg", el => el.outerHTML);
   writeFile('./img/generated/sparklines/sparkline-tests.svg',sparklineTests);
+  console.log('got tests');
 
   const postvaxCases = await page.$eval("cagov-chart-dashboard-postvax-chart-prerender[data-chart-config-key='cases'] svg", el => el.outerHTML);
   writeFile('./img/generated/postvax/postvax-cases.svg',postvaxCases);
+  console.log('got vax cases');
 
   const postvaxDeaths = await page.$eval("cagov-chart-dashboard-postvax-chart-prerender[data-chart-config-key='deaths'] svg", el => el.outerHTML);
   writeFile('./img/generated/postvax/postvax-deaths.svg',postvaxDeaths);
+  console.log('got vax deaths');
 
   const postvaxHospitalizations = await page.$eval("cagov-chart-dashboard-postvax-chart-prerender[data-chart-config-key='hospitalizations'] svg", el => el.outerHTML);
   writeFile('./img/generated/postvax/postvax-hospitalizations.svg',postvaxHospitalizations);
+  console.log('got vax hospitalizations');
 
 
   await browser.close();
